d4d6371cd244fd4dbf96984cd62740138c627864
Alex Zhang <zhangtongcao@qq.com>
2019-03-05 16:40:51 +0800

    first commit

---
+++ src/main/java/com/yumong/orange/IdWorker.java
@@ -0,0 +1,82 @@
+package com.yumong.orange;
+
+/**
+ * Twitter Snowflake : A distributed ID generator.
+ *
+ * Detail https://www.callicoder.com/distributed-unique-id-sequence-number-generator/
+ * Twitter official repo: https://github.com/twitter-archive/snowflake/tree/snowflake-2010
+ *
+ */
+public class IdWorker {
+
+    private static IdWorker idWorker = null;
+    private final long twepoch = 1420041600000L;
+    private final long workerIdBits = 5L;
+    private final long datacenterIdBits = 5L;
+    private final long maxWorkerId = 31L;
+    private final long maxDatacenterId = 31L;
+    private final long sequenceBits = 12L;
+    private final long workerIdShift = 12L;
+    private final long datacenterIdShift = 17L;
+    private final long timestampLeftShift = 22L;
+    private final long sequenceMask = 4095L;
+    private long workerId;
+    private long datacenterId;
+    private long sequence = 0L;
+    private long lastTimestamp = -1L;
+
+    public IdWorker(long workerId, long datacenterId) {
+        if (workerId <= 31L && workerId >= 0L) {
+            if (datacenterId <= 31L && datacenterId >= 0L) {
+                this.workerId = workerId;
+                this.datacenterId = datacenterId;
+            } else {
+                throw new IllegalArgumentException(String.format("datacenter Id can't be greater than %d or less than 0", 31L));
+            }
+        } else {
+            throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0", 31L));
+        }
+    }
+
+    public IdWorker() {
+    }
+
+    public synchronized long nextId() {
+        long timestamp = this.timeGen();
+        if (timestamp < this.lastTimestamp) {
+            throw new RuntimeException(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", this.lastTimestamp - timestamp));
+        } else {
+            if (this.lastTimestamp == timestamp) {
+                this.sequence = this.sequence + 1L & 4095L;
+                if (this.sequence == 0L) {
+                    timestamp = this.tilNextMillis(this.lastTimestamp);
+                }
+            } else {
+                this.sequence = 0L;
+            }
+
+            this.lastTimestamp = timestamp;
+            return timestamp - 1420041600000L << 22 | this.datacenterId << 17 | this.workerId << 12 | this.sequence;
+        }
+    }
+
+    protected long tilNextMillis(long lastTimestamp) {
+        long timestamp;
+        for(timestamp = this.timeGen(); timestamp <= lastTimestamp; timestamp = this.timeGen()) {
+        }
+
+        return timestamp;
+    }
+
+    protected long timeGen() {
+        return System.currentTimeMillis();
+    }
+
+    public static long ID() {
+        return idWorker.nextId();
+    }
+
+    static {
+        idWorker = new IdWorker();
+    }
+}
